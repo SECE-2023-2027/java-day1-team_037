2c7d4e105cf7271812f29b99302941937f92f5ec
sanju2512 <sanju2512005@gmail.com>
2026-06-16 12:10:00 +0530

    6

---
+++ Q6.java
@@ -0,0 +1,17 @@
+public class Q6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.print("Input a decimal number: ");
+        int decimal = sc.nextInt();
+
+        // Conversion
+        String hex = Integer.toHexString(decimal).toUpperCase();
+
+        // Output
+        System.out.println("Hexadecimal number is : " + hex);
+    }
+}
+
